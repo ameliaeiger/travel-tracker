54e4d66037eb11a5730468709fd91db473cf7433
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-06-12 17:38:25 -0600

    Add micromodal library. Add methods to display traveler data to DOM

--- src/TravelerRepo.js
+++ src/TravelerRepo.js
@@ -7,9 +7,6 @@ class TravelerRepo {
     getTraveler(id){
         return this.travelers.find(traveler => traveler.id === id)
     }
-    createNewTraveler(id, data) {
-        return new Traveler(id, data)
-    }
 }
 
 export default TravelerRepo;
--- src/scripts.js
+++ src/scripts.js
@@ -3,61 +3,69 @@
 
 // Libraries
 const dayjs = require('dayjs');
+import MicroModal from 'micromodal'; 
+
 
 // Imports
 import { allTravelersData, allTripsData, allDestinationsData } from './apiCalls';
-import './css/styles.css';
-import './images/turing-logo.png'
 import DestinationRepo from './DestinationRepo';
 import Destination from './Destination';
 import TravelerRepo from './TravelerRepo';
 import Traveler from './Traveler';
 import TripRepo from './TripRepo.js';
+import './css/styles.css';
+import './images/turing-logo.png'
 
-// GLOBALs
-let travelerTrips = document.getElementById("traveler-trips-display-past");
-let destinationSelect = document.getElementById("destination-options");
-let startDate = document.getElementById("date-input-start");
-let endDate = document.getElementById("date-input-end");
-let destinationInput = document.getElementById("destination-options");
-let numTravelers = document.getElementById("num-travelers");
-let submit = document.getElementById("submit");
+
+// GLOBALS
+
+let currentUserID;
+let travelerRepo;
+let tripRepo;
+let destRepo;
+
+const travelerPastTrips = document.getElementById("traveler-trips-display-past");
+const destinationSelect = document.getElementById("destination-options");
+
+//---       Form
+const startDate = document.getElementById("date-input-start");
+const endDate = document.getElementById("date-input-end");
+const destinationInput = document.getElementById("destination-options");
+const numTravelers = document.getElementById("num-travelers");
+const submit = document.getElementById("submit");
+//---       Proposed Trip
+const proposedTripTotal = document.getElementById("total-cost");
+const lodgingCost = document.getElementById("lodging-cost");
+const proposedTripSum = document.getElementById("trip-sum");
+const flightCost = document.getElementById("flight-cost");
+
+//---       Login
+const requestLogin = document.getElementById("request-login");
+const login = document.getElementById("login-button");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const welcomeUser = document.getElementById("welcome-user");
+
 
 // Event Listeners
-
+window.addEventListener("load", toggleLogin);
 submit.addEventListener("click", submitTrip);
+requestLogin.addEventListener("click", toggleLogin);
+login.addEventListener("click", userLogin);
 
-
-function submitTrip(e){
-    e.preventDefault()
-    //  Date
-    let start = dayjs(startDate.value);
-    let end = dayjs(endDate.value);
-    console.log(startDate.value, endDate.value);
-    let duration = end.diff(start, "day");
-    console.log(duration);
-
-    // Destination
-    let dest = destinationInput.value;
-    console.log(dest)
-
-    // Number of travelers
-    let num = numTravelers.value;
-    console.log(num);
-};
-
-
+// Promise All
 Promise.all([allTravelersData, allTripsData, allDestinationsData])
     .then(data => {
 
-        let travelersRepo = new TravelerRepo(data[0].travelers);
-        let tripsRepo = new TripRepo(data[1].trips);
-        let destinationsRepo = new DestinationRepo(data[2].destinations);
+        travelerRepo = new TravelerRepo(data[0].travelers);
+        tripRepo = new TripRepo(data[1].trips);
+        destRepo = new DestinationRepo(data[2].destinations);
+
+        addDestinationOptions(destRepo);
 
 
-        travelerDataHelper(travelersRepo, tripsRepo, destinationsRepo);
+        // travelerDataHelper(travelersRepo, tripsRepo, destinationsRepo);
 
-        addDestinationOptions(destinationsRepo);
 
         // console.log("----Travelers Repository: ", travelersRepo);
         // console.log("----Destinations Repository: ", destinationsRepo);
@@ -65,22 +73,132 @@ Promise.all([allTravelersData, allTripsData, allDestinationsData])
     })
 
 
+// MicroModal
+function toggleLogin() {
+    MicroModal.show("modal-1");
+    // MicroModal.close("modal-1")
+}
+
+
+function userLogin() {
+    if (username.value == "traveler50" && password.value == "travel"){
+        currentUserID = 50;
+        // allTravelersData.then(data => {
+        //     let travelerObj = data.travelers.find(traveler => traveler.id == currentUserID);
+        //     let allTrips = tripRepo.returnAllUserTrips(travelerObj.id);
+        //     let newOne = new Traveler (travelerObj.id, travelerObj.name, travelerObj.travelerType, allTrips);
+        //     renderDisplay(tripRepo, destRepo, newOne);
+        // });
+
+        console.log(destRepo, travelerRepo, tripRepo)
+
+        let currentUser = travelerRepo.getTraveler(currentUserID);
+        let allTrips = tripRepo.returnAllUserTrips(currentUser.id);
+        let traveler = new Traveler(currentUser.id, currentUser.name, currentUser.travelerType, allTrips, destRepo);
+        renderDisplay(traveler, destRepo, tripRepo);
+
+
+
+        MicroModal.close("modal-1");
+
+
+
+
+
+    } else if (username.value && password.value){
+        currentUserID = Math.floor(Math.random() * 50);
+        allTravelersData.then(data => {
+            let travelerObj = data.travelers.find(traveler => traveler.id == currentUserID);
+            let newOne = new Traveler (travelerObj.id, travelerObj.name, travelerObj.travelerType);
+            renderTraveler(newOne);
+        });
+        MicroModal.close("modal-1");
+    };
+};
+
+function renderDisplay(traveler, destRepo, tripRepo) {
+    renderTraveler(traveler, destRepo)
+}
+
+function renderTraveler(travObj, destRepo) {
+    welcomeUser.innerHTML = `Welcome, ${travObj.name}!`;
+    let keys = travObj.pastTripKeys;
+    let allPastDestinations = destRepo.getDestById(keys);
+    createImageNodes(allPastDestinations);
+};
+
+
+function submitTrip(e) {
+    e.preventDefault()
+    //  Date
+    let start = dayjs(startDate.value);
+    let end = dayjs(endDate.value);
+    let duration = end.diff(start, "day");
+
+    // Destination
+    let dest = destinationInput.value;
+
+    // Number of travelers
+    let num = numTravelers.value;
+
+    // Trip Object
+    let newTrip = {
+        "duration": duration,
+        "destination": dest,
+        "travelers": num
+    }
+    allDestinationsData.then(data => {
+        data.destinations;
+        let costObj = findCost(data.destinations, newTrip);
+        console.log(costObj);
+        flightCost.innerHTML = `Flight Cost: ${costObj.flight}`;
+        lodgingCost.innerHTML = `Lodging Cost: ${costObj.lodging}`;
+        proposedTripSum.innerHTML = `Cost: ${costObj.sum}`;
+        proposedTripTotal.innerHTML = `Total + Agent's Fee: ${costObj.sumFee}`;
+    });
+};
+
+function findCost(data, newTrip) {
+    console.log(data, newTrip);
+    let match = data.filter(destination => destination.destination == newTrip.destination);
+    let flightCost = match[0].estimatedFlightCostPerPerson * newTrip.travelers;
+    let lodgingCost = match[0].estimatedLodgingCostPerDay * newTrip.duration;
+    let sum = flightCost + lodgingCost;
+    let sumFee =  (flightCost + lodgingCost) * 1.1;
+    let object = {
+        "flight": flightCost,
+        "lodging": lodgingCost,
+        "sum": (Math.round(sum * 10) / 10),
+        "sumFee": (Math.round(sumFee * 10) / 10)
+    }
+    return object;
+}
+
+
+
+
     // TRAVELER
 
     function travelerDataHelper(travelersRepo, tripsRepo, destinationsRepo){
-        let travelerData = tripsRepo.returnAllUserTrips(5);
-        let traveler = travelersRepo.createNewTraveler(5, travelerData);
+        let name = travelersRepo.getTraveler(currentUserID).name;
+        let travelerData = tripsRepo.returnAllUserTrips(currentUserID);
+        let traveler = new Traveler(currentUserID, name, travelerData);
+
+        console.log(travelersRepo)
+        console.log(traveler)
+
+
         let keys = traveler.getDestinationIDs();
         let travelerDestinations = destinationsRepo.getDestById(keys);
         createImageNodes(travelerDestinations);
 
         // TESTING: findPastTrips -- not complete. save for later use.
         traveler.findPastTrips();
-
     }
 
-    function createImageNodes(travelerDestinations) {
-        travelerDestinations.forEach(function(destination, index){
+    function createImageNodes(pastTrips) {
+        console.log(pastTrips)
+        pastTrips.forEach(function(destination, index){
             // Create element for each image
             let imageNode = document.createElement("img");
             let classString = `img${index}`
@@ -88,7 +206,8 @@ Promise.all([allTravelersData, allTripsData, allDestinationsData])
             imageNode.classList.add("traveler-image");
             imageNode.src = destination.image;
             imageNode.alt = destination.alt;
-            travelerTrips.appendChild(imageNode);
+            console.log(imageNode)
+            travelerPastTrips.appendChild(imageNode);
 
             // Create element for text -- save for later use
             // let text = `Destination: ${destination.destination} Image: ${destination.image} Alt: ${destination.alt}`
