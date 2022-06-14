156e7a798015405d7d6245177166bf90e0f038d0
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-06-14 10:13:54 -0600

    Refactor

--- src/Trip.js
+++ src/Trip.js
@@ -1,5 +1,35 @@
-class Trip {
-    constructor() {
+const dayjs = require("dayjs");
 
+class Trip {
+    constructor(tripData) {
+        this.id = tripData.id;
+        this.userID = tripData.userID;
+        this.destinationID = tripData.destinationID;
+        this.travelers = tripData.travelers;
+        this.date = tripData.date;
+        this.duration = tripData.duration;
+        this.status = tripData.status;
+        this.suggestedActivities = [];
+        this.tripCost = 0;
+        this.category = "";
     }
+    getTripCost(destination) {
+        let sumLodging = destination.estimatedLodgingCostPerDay * this.duration;
+        let sumFlight = destination.estimatedFlightCostPerPerson * this.travelers;
+        let totalSum = sumLodging + sumFlight;
+        this.tripCost = totalSum * 1.1;
+      };
+      getTripCategory = () => {
+        if (this.status === "pending") {
+          return (this.category = "pending");
+        } else if (dayjs().isAfter(dayjs(this.date))) {
+          return (this.category = "past");
+        } else if (dayjs().isSame(dayjs(this.date))) {
+          return (this.category = "present");
+        } else if (dayjs().isBefore(dayjs(this.date))) {
+          return (this.category = "upcoming");
+        }
+      };
 }
+
+export default Trip;
---
+++ test/traveler-repo-test.js
@@ -0,0 +1,49 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+import TravelerRepo from '../src/TravelerRepo.js';
+
+describe('TravelerRepository', () => {
+    
+    let travelerRepo
+    let travelers
+
+
+
+    beforeEach( () => {
+        travelers= [
+            {
+            id: 1,
+            name: "Ham Leadbeater",
+            travelerType: "relaxer"
+            },
+            {
+            id: 2,
+            name: "Rachael Vaughten",
+            travelerType: "thrill-seeker"
+            },
+            {
+            id: 3,
+            name: "Sibby Dawidowitsch",
+            travelerType: "shopper"
+            },
+        ],
+
+        travelerRepo = new TravelerRepo(travelers)
+
+  });
+
+
+    it('should be a function', function () {
+        expect(TravelerRepo).to.be.a('function');
+    });
+
+    it('should return a traveler when given an ID', function () {
+        expect(travelerRepo.getTraveler(1)).to.deep.equal(travelers[0]);
+    });
+
+    // it('should have a name', function () {
+    //     expect(traveler.name).to.equal("Beth");
+    // });
+
+});
